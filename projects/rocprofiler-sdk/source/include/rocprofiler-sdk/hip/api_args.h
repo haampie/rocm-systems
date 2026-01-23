7c3e0e68c6c4864c0d1c9b15c022c3b5561f629b
Sebastian Luzynski <Sebastian.Luzynski@amd.com>
2026-01-23 08:55:10 +0000

    SWDEV-546303 - implement hipKernelGetAttribute API
    Signed-off-by: Sebastian Luzynski <Sebastian.Luzynski@amd.com>

--- projects/rocprofiler-sdk/source/include/rocprofiler-sdk/hip/api_args.h
+++ projects/rocprofiler-sdk/source/include/rocprofiler-sdk/hip/api_args.h
@@ -3411,6 +3411,15 @@ typedef union rocprofiler_hip_api_args_t
         hipMemAllocationType type;
     } hipMemGetMemPool;
 #endif
+#if HIP_RUNTIME_API_TABLE_STEP_VERSION >= 23
+    struct
+    {
+        int*                  pi;
+        hipFunction_attribute attrib;
+        hipKernel_t           kernel;
+        hipDevice_t           dev;
+    } hipKernelGetAttribute;
+#endif
 } rocprofiler_hip_api_args_t;
 
 ROCPROFILER_EXTERN_C_FINI

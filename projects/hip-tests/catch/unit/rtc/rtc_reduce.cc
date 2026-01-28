d2b64815073d6f35ac0e7b0df4e79db038b4b3cb
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-01-28 15:41:10 +0000

    SWDEV-539306 - use LinearAllocGuard

--- projects/hip-tests/catch/unit/rtc/rtc_reduce.cc
+++ projects/hip-tests/catch/unit/rtc/rtc_reduce.cc
@@ -46,13 +46,12 @@ void runRtcReduceOp(hiprtcProgram& prog, T* output, const T* input, const MaskTy
   const char* loweredName;
   hipFunction_t kernel;
   hipModule_t module;
-  int* d_numReduces;
+  LinearAllocGuard<int> d_numReduces(LinearAllocs::hipMalloc, sizeof(int));
 
-  HIP_CHECK(hipMalloc(&d_numReduces, sizeof(int)));
-  HIP_CHECK(hipMemcpy(d_numReduces, &numReduces, sizeof(int), hipMemcpyHostToDevice));
+  HIP_CHECK(hipMemcpy(d_numReduces.ptr(), &numReduces, sizeof(int), hipMemcpyHostToDevice));
   std::vector<const void*> args = {
       reinterpret_cast<const void*>(output), reinterpret_cast<const void*>(input),
-      reinterpret_cast<const void*>(masks), reinterpret_cast<void*>(d_numReduces)};
+      reinterpret_cast<const void*>(masks), reinterpret_cast<void*>(d_numReduces.ptr())};
   std::size_t sizeBytes = args.size() * sizeof(void*);
   void* config[] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, args.data(), HIP_LAUNCH_PARAM_BUFFER_SIZE,
                     &sizeBytes, HIP_LAUNCH_PARAM_END};

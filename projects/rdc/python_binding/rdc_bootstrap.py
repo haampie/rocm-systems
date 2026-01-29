6c02df2f8bd618eaedb740eb0bb6b5af1876f50f
yalmusaf_amdeng <Yazen.ALMusaffar@amd.com>
2026-01-29 14:48:49 -0600

    added new errors returned
    Signed-off-by: yalmusaf_amdeng <Yazen.ALMusaffar@amd.com>

--- projects/rdc/python_binding/rdc_bootstrap.py
+++ projects/rdc/python_binding/rdc_bootstrap.py
@@ -59,6 +59,8 @@ class rdc_status_t(Enum):
      RDC_ST_NO_DATA = 13
      RDC_ST_PERM_ERROR = 14
      RDC_ST_CORRUPTED_EEPROM = 15
+     RDC_ST_GROUP_NOT_FOUND = 16
+     RDC_ST_FLDGROUP_NOT_FOUND = 17
      RDC_ST_UNKNOWN_ERROR = 4294967295
 
 class rdc_operation_mode_t(c_int):

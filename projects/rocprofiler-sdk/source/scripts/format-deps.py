6cf4859aa1e93a11a939bbbded1cd2f49d3605a1
Mythreya <mythreya.kuricheti@amd.com>
2026-02-11 21:51:36 +0000

    Apply formatting

--- projects/rocprofiler-sdk/source/scripts/format-deps.py
+++ projects/rocprofiler-sdk/source/scripts/format-deps.py
@@ -114,11 +114,13 @@ class FormatAll(argparse.Action):
 
 class InstallDepsUbuntu(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        os.system("sudo apt-get update; \
+        os.system(
+            "sudo apt-get update; \
             sudo apt-get install -y python3-pip software-properties-common wget curl clang-format-11; \
             python3 -m pip install -U cmake-format; \
             python -m pip install --upgrade pip; \
-            python -m pip install black")
+            python -m pip install black"
+        )
         exit(0)
 
 
@@ -143,6 +145,10 @@ parser.add_argument(
     "-cd", "--counter-defs", nargs=0, help="format counter_defs.yaml", action=FormatYAML
 )
 parser.add_argument(
-    "-a", "--all", nargs=0, help="format cmake, source, python, and yaml files", action=FormatAll
+    "-a",
+    "--all",
+    nargs=0,
+    help="format cmake, source, python, and yaml files",
+    action=FormatAll,
 )
 parser.parse_args()
